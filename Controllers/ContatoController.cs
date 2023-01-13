9198bcfc23804a12f531c937b4931f03fd3e6182
Glyson <kauaglyson@gmail.com>
2023-01-13 15:54:30 -0300

    Criando a controller de contato

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProcessoSeletivoBLUE.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        
+    }
+}
