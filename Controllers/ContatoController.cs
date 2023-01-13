6b2072e3788dc19b0fff613d8f858ff31f0dc2a6
Glyson <kauaglyson@gmail.com>
2023-01-13 16:23:44 -0300

    Atualizar um contato (UPDATE)

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -14,13 +14,13 @@ namespace ProcessoSeletivoBLUE.Controllers
     {
         // recebe o banco de dados através do construtor e atribui à _context
         private readonly AgendaContext _context;
-        public ContatoController (AgendaContext context)
+        public ContatoController(AgendaContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public IActionResult Read ()
+        public IActionResult Read()
         {
             var contatos = _context.Contatos.ToList();
             if (!contatos.Any())
@@ -29,11 +29,26 @@ namespace ProcessoSeletivoBLUE.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create (Contato contato)
+        public IActionResult Create(Contato contato)
         {
             _context.Contatos.Add(contato);
             _context.SaveChanges();
             return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Contato contato)
+        {
+            var ContatoBanco = _context.Contatos.Find(id);
+            
+            ContatoBanco.Nome = contato.Nome;
+            ContatoBanco.Email = contato.Email;
+            ContatoBanco.Numero = contato.Numero;
+
+            _context.Contatos.Update(ContatoBanco);
+            _context.SaveChanges();
+
+            return Ok(ContatoBanco);
         }
     }
 }
