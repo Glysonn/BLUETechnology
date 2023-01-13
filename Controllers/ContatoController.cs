d7233c2b53f14ab467c152bb2ad4e67540e69419
Glyson <kauaglyson@gmail.com>
2023-01-13 16:14:46 -0300

    Listar todos os contatos (READ)

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProcessoSeletivoBLUE.Context;
+using ProcessoSeletivoBLUE.Models;
 
 namespace ProcessoSeletivoBLUE.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class ContatoController : ControllerBase
     {
-        
+        // recebe o banco de dados através do construtor e atribui à _context
+        private readonly AgendaContext _context;
+        public ContatoController (AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Read ()
+        {
+            var contatos = _context.Contatos.ToList();
+            if (!contatos.Any())
+                return NotFound("Não há contatos");
+            return Ok(contatos);
+        }
     }
 }
