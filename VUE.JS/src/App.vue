778cf96e344d8c93c1a8a4f40971d8702b8ea39c
Glyson <kauaglyson@gmail.com>
2023-01-16 18:23:14 -0300

    Adicionar contato

--- VUE.JS/src/App.vue
+++ VUE.JS/src/App.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-sm bg-light navbar-light">
       <ul class="navbar-nav">
         <li class="nav-item">
-        <router-link to="/" class="nav-link">Home</router-link>
+        <router-link to="/" class="nav-link">Contatos</router-link>
         </li>
         <li class="nav-item">
         <router-link to="/adicionar" class="nav-link">Adicionar</router-link>
--- VUE.JS/src/components/FormAddContato.vue
+++ VUE.JS/src/components/FormAddContato.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="form-group">
         <h2>Adicionar novo contato</h2>
-        <form class="was-validated">
+        <form class="was-validated" @submit.prevent="postData">
             <label for="NomeContato"> Nome</label>  <br>
-                <input type="text" name="NomeContato" id="NomeContato" class="form-control" required>
+                <input type="text" name="NomeContato" id="NomeContato" v-model="form.nome" class="form-control" placeholder="Ex.: Nome" pattern="[a-zA-Z\s]+" required>
             <br>
             <label for="EmailContato"> Email</label>  <br>
-                <input type="text" name="EmailContato" id="EmailContato" class="form-control" required>
+                <input type="email" name="EmailContato" id="EmailContato" v-model="form.email"  class="form-control" placeholder="Ex.: email@mail.com" required>
             <br>
             <label for="TelefoneContato"> Telefone</label>  <br>
-                <input type="tel" name="TelefoneContato" id="TelefoneContato" class="form-control" required>
+                <input type="tel" name="TelefoneContato" id="TelefoneContato" v-model="form.numero"  class="form-control" placeholder= "Ex.: (81) 999999999" pattern="^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$" required>
             <br>
             <input type="submit" value="Adicionar contato" class="btn btn-primary"/>
         </form>
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 
 export default{
     name: 'FormAddContato',
@@ -30,10 +31,16 @@ export default{
         }
     },
     methods: {
-        postData() {
+        async postData() {
             const apiUrl = "https://localhost:7296/Contato";
-
+            try {
+                await axios.post(apiUrl, this.form)
+                alert("Contato salvo!");
+            } catch (error) {
+                console.error(error)
+            }
         }
+
     }
 }
 </script>
