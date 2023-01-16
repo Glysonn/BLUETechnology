ab5a201a3cb75388eb6dc6ddb0ae54d42d82c85c
Glyson <kauaglyson@gmail.com>
2023-01-16 13:04:31 -0300

    Componente: Corpo da tabela com dados da api

---
+++ VUE.JS/src/components/TBodyContatos.vue
@@ -0,0 +1,40 @@
+<template>
+    <tbody>
+        <tr v-for="pessoa in contatos" :key="pessoa.id">
+            <th scope="row">{{ pessoa.id }}</th>
+            <td>{{ pessoa.nome }}</td>
+            <td>{{ pessoa.email }}</td>
+            <td>{{ pessoa.numero }}</td>
+            <td>
+                <a>Detalhes</a> &nbsp;
+                <a>Editar</a> &nbsp;
+                <a>Apagar</a> &nbsp;
+            </td>
+        </tr>
+    </tbody>
+</template>
+
+<script>
+export default {
+    name: 'TBodyContatos',
+    data () {
+        return {
+            contatos: []
+        }
+    },
+    created(){
+        this.getData()
+    },
+    methods:{
+        getData() {
+            const apiUrl = 'https://localhost:7296/contato';
+            fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                this.contatos = data.sort()
+            })
+            .catch('Erro ao carregar dados:');
+            }
+    }
+}
+</script>
