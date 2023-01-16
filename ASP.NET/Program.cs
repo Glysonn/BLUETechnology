c57e41a2c4ac2e7300b0efe39eeb8b02ebd7ef81
Glyson <kauaglyson@gmail.com>
2023-01-16 12:19:44 -0300

    Configurando o CORS para aceitar requisições

--- ASP.NET/Program.cs
+++ ASP.NET/Program.cs
@@ -1,13 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using ProcessoSeletivoBLUE.Context;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+// configurando o CORS para permitir as requisições do frontend
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.WithOrigins("http://localhost:8080")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
+});
 
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 // adicionando o banco em memória do entity framework na API
 builder.Services.AddDbContext<AgendaContext> (options => options.UseInMemoryDatabase("Agenda"));
@@ -20,6 +33,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
