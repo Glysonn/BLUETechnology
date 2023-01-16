959a49e8d16a0c93d6caaeb9d66bc8bd47eb9adc
Glyson <kauaglyson@gmail.com>
2023-01-15 22:26:02 -0300

    Adicionando a Home view

---
+++ src/components/BtnAddContato.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <router-link to="/adicionar">
+            <button class="btn btn-secondary">Adicionar novo contato</button>
+        </router-link> 
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'BtnAddContato'
+}
+</script>
---
+++ src/components/TabelaContatos.vue
@@ -0,0 +1,29 @@
+<!-- a tabela deve ser preenchida com os dados retornados do endpoind. Dados atuais somente para demonstração-->
+<template>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Telefone</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Glyson</td>
+                    <td>kauaglyson@gmail.com</td>
+                    <td>(81) 983723417</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabelaContatos'
+}
+</script>
