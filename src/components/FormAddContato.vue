06503e1532516cc92ed392b92e30592470cc4871
Glyson <kauaglyson@gmail.com>
2023-01-15 22:34:15 -0300

    Tela de adicionar contato

---
+++ src/components/FormAddContato.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="form-group">
+        <h2>Adicionar novo contato</h2>
+        <form class="was-validated">
+            <label for="NomeContato"> Nome</label>  <br>
+                <input type="text" name="NomeContato" id="NomeContato" class="form-control" required>
+            <br>
+            <label for="EmailContato"> Email</label>  <br>
+                <input type="text" name="EmailContato" id="EmailContato" class="form-control" required>
+            <br>
+            <label for="TelefoneContato"> Telefone</label>  <br>
+                <input type="tel" name="TelefoneContato" id="TelefoneContato" class="form-control" required>
+            <br>
+            <input type="submit" value="Adicionar contato" class="btn btn-primary"/>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'FormAddContato',
+}
+</script>
---
+++ src/views/AdicionarView.vue
@@ -0,0 +1,13 @@
+<template>
+    <FormAddContato />
+</template>
+
+<script>
+import FormAddContato from '@/components/FormAddContato.vue';
+export default{
+    name: 'AdicionarView',
+    components: {
+        FormAddContato
+    }
+}
+</script>
